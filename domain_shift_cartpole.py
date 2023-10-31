c04029ddee329ea30507cfcd2eb69e9b986e7c8c
Dylan Forde <df21@csg25-17.ucc.ie>
2023-10-31 18:25:16 +0000

    working code however lr scheduler is about to be changed

--- domain_shift_cartpole.py
+++ domain_shift_cartpole.py
@@ -1,5 +1,19 @@
 import gym
 from gym.envs.classic_control import CartPoleEnv
+import gymnasium as gym
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import optuna
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class CustomCartPoleEnv(CartPoleEnv):
     def __init__(self):
@@ -21,21 +35,6 @@ class CustomCartPoleEnv(CartPoleEnv):
         self.length = self.original_length  # Reset the pole length when the environment is reset
         return super().reset()
 
-import gymnasium as gym
-import math
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-from collections import namedtuple, deque
-from itertools import count
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import optuna
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
@@ -56,21 +55,20 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(n_observations, 256)
         self.layer2 = nn.Linear(256, 256)
-        self.layer3 = nn.Linear(256, n_actions)
-        self.bn1 = nn.BatchNorm1d(256)
-        self.bn2 = nn.BatchNorm1d(256)
+        self.layer3 = nn.Linear(256, 256)
+        self.layer4 = nn.Linear(256, n_actions)
         self.init_weights()
     
     def forward(self, x):
-        x = F.relu(self.bn1(self.layer1(x)))
-        x = F.relu(self.bn2(self.layer2(x)))
-        return self.layer3(x)
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.relu(self.layer3(x))
+        return self.layer4(x)
     
     def init_weights(self):
-        for layer in [self.layer1, self.layer2, self.layer3]:
+        for layer in [self.layer1, self.layer2, self.layer3, self.layer4]:
             nn.init.xavier_uniform_(layer.weight)
             nn.init.constant_(layer.bias, 0)
-
 
 def set_seed(seed):
     np.random.seed(seed)
@@ -85,15 +83,15 @@ global best_value
 best_value = -float('inf')
 
 def objective(trial):
-    env = gym.make('CartPole-v1')
+    env = CustomCartPoleEnv()
     global best_value
     # Define the hyperparameter search space
-    LR = trial.suggest_float('lr', 1e-4, 1e-2, log=True)
+    LR = trial.suggest_float('lr', 1e-4, 1e-3, log=True)
     GAMMA = trial.suggest_float('gamma', 0.95, 0.999)
-    TARGET_UPDATE = trial.suggest_int('target_update', 5, 20)
+    TARGET_UPDATE = trial.suggest_int('target_update', 500, 2000)
     EPS_START = trial.suggest_float('eps_start', 0.8, 1)
-    EPS_END = trial.suggest_float('eps_end', 0.01, 0.1)
-    EPS_DECAY = trial.suggest_int('eps_decay', 200, 1000)
+    EPS_END = trial.suggest_float('eps_end', 0.05, 0.05)
+    EPS_DECAY = trial.suggest_int('eps_decay', 2000, 10000)
     BATCH_SIZE = trial.suggest_categorical('batch_size', [32, 64, 128, 256, 512])
     REPLAY_MEMORY_SIZE = trial.suggest_categorical('replay_memory_size', [1000, 2000, 3000, 4000, 5000])
     
@@ -115,6 +113,7 @@ def objective(trial):
         sample = random.random()
         eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
         steps_done += 1
+        eps_thresholds.append(eps_threshold)  # Store the epsilon value
         
         # Set the model to evaluation mode
         policy_net.eval()
@@ -126,6 +125,9 @@ def objective(trial):
                 return torch.tensor([[env.action_space.sample()]], dtype=torch.long, device=device)
 
     episode_durations = []
+
+    losses = []
+    eps_thresholds = []
 
     def optimize_model():
         if len(memory) < BATCH_SIZE:
@@ -150,6 +152,7 @@ def objective(trial):
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
         loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+        losses.append(loss.item())  # Store the loss value
 
         optimizer.zero_grad()
         loss.backward()
@@ -161,23 +164,38 @@ def objective(trial):
         scheduler.step(np.mean(episode_durations))
 
     # Function to plot the duration of each episode
-    def plot_duration(episode_durations, optimization_mode=False):
+    def plot_duration(episode_durations, losses, eps_thresholds, optimization_mode=False):
         if optimization_mode:
             return
-        plt.figure(2)
+
+        plt.figure(1)
         plt.clf()
+
+        # Plot Episode Durations
+        plt.subplot(3, 1, 1)
         durations_t = torch.tensor(episode_durations, dtype=torch.float)
-        plt.title('Training...')
-        plt.xlabel('Episode')
+        plt.title('Training Metrics')
         plt.ylabel('Duration')
         plt.plot(durations_t.numpy())
         if len(durations_t) >= 100:
             means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
             means = torch.cat((torch.zeros(99), means))
             plt.plot(means.numpy())
+
+        # Plot Losses
+        plt.subplot(3, 1, 2)
+        plt.ylabel('Loss')
+        plt.plot(losses)
+
+        # Plot Epsilon Thresholds
+        plt.subplot(3, 1, 3)
+        plt.xlabel('Episode/Step')
+        plt.ylabel('Epsilon')
+        plt.plot(eps_thresholds)
+
         plt.pause(0.001)  # pause a bit so that plots are updated
 
-    num_episodes = 2500
+    num_episodes = 25000
     for i_episode in range(num_episodes):
         state, info = env.reset()
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
@@ -212,7 +230,7 @@ def objective(trial):
         if i_episode % TARGET_UPDATE == 0:
             target_net.load_state_dict(policy_net.state_dict())
         
-        plot_duration(episode_durations, optimization_mode=True)
+        plot_duration(episode_durations, losses, eps_thresholds, optimization_mode=False)
 
         trial.report(episode_durations[-1], i_episode)
 
@@ -229,8 +247,9 @@ storage_url = "sqlite:///optuna_study.db"
 study_name = 'cartpole_study'
 
 # Create a new study or load an existing study
-study = optuna.create_study(study_name=study_name, storage=storage_url, direction='maximize', load_if_exists=True)
-study.optimize(objective, n_trials=300)
+pruner = optuna.pruners.PercentilePruner(95)  # Prune if the trial's best intermediate result is in the bottom 5% of intermediate results.
+study = optuna.create_study(study_name=study_name, storage=storage_url, direction='maximize', load_if_exists=True, pruner=pruner)
+study.optimize(objective, n_trials=5)
 
 # Load the study
 study = optuna.load_study(study_name=study_name, storage=storage_url)
