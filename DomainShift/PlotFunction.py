63218a05b1c362bfcd42ca47c2860e95b1c111cf
Dylan Forde <df21@csg25-17.ucc.ie>
2023-11-06 18:35:32 +0000

    moved plot function to separate file

---
+++ DomainShift/PlotFunction.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+# Initialize the figure and axes for the plots outside of the function
+fig, axs = plt.subplots(4, 1, figsize=(10, 7))
+
+
+def plot_function(episode_durations, losses, eps_thresholds, episode_rewards, optimization_mode=False):
+        if optimization_mode:
+            return
+
+        # Clear the current axes and figure
+        for ax in axs:
+            ax.cla()
+
+        # Plot Episode Durations
+        axs[0].set_title('Training Metrics')
+        axs[0].set_ylabel('Duration')
+        durations_t = torch.tensor(episode_durations, dtype=torch.float)
+        axs[0].plot(durations_t.numpy(), label='Raw')
+        smoothed_durations = np.cumsum(durations_t.numpy()) / (np.arange(len(durations_t)) + 1)
+        axs[0].plot(smoothed_durations, color='orange', label='Smoothed')
+        axs[0].legend()
+
+        # Plot Losses
+        axs[1].set_ylabel('Loss')
+        losses_t = np.array(losses)
+        axs[1].plot(losses_t, label='Raw')
+        smoothed_losses = np.cumsum(losses_t) / (np.arange(len(losses_t)) + 1)
+        axs[1].plot(smoothed_losses, color='orange', label='Smoothed')
+        axs[1].legend()
+
+        # Plot Epsilon Thresholds
+        axs[2].set_ylabel('Epsilon')
+        axs[2].plot(eps_thresholds, label='Epsilon')
+        axs[2].legend()
+
+        # Plot Reward
+        axs[3].set_xlabel('Episode')
+        axs[3].set_ylabel('Reward')
+        rewards_t = torch.tensor(episode_rewards, dtype=torch.float)
+        axs[3].plot(rewards_t.numpy(), label='Episode Reward')
+        axs[3].legend()
+
+        plt.tight_layout()
+        plt.pause(0.001)  # pause a bit so that plots are updated
--- DomainShift/domain_shift_cartpole.py
+++ DomainShift/domain_shift_cartpole.py
@@ -1,8 +1,6 @@
-import gymnasium as gym
 import math
 import random
 import numpy as np
-import matplotlib.pyplot as plt
 from itertools import count
 import torch
 import torch.optim as optim
@@ -13,6 +11,7 @@ import optuna
 from DomainShift.CustomCartPoleEnvironmentClass import CustomCartPoleEnv
 from ReplayMemoryClass import ReplayMemory, Transition
 from DQNClass import DQN
+from PlotFunction import plot_function
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -110,50 +109,7 @@ def objective(trial):
         for target_param, policy_param in zip(target_net.parameters(), policy_net.parameters()):
             target_param.data.copy_(TAU * policy_param.data + (1.0 - TAU) * target_param.data)
         
-
-    # Initialize the figure and axes for the plots outside of the function
-    fig, axs = plt.subplots(4, 1, figsize=(10, 7))
-
-    def plot_durations(episode_durations, losses, eps_thresholds, episode_rewards, optimization_mode=False):
-        if optimization_mode:
-            return
-
-        # Clear the current axes and figure
-        for ax in axs:
-            ax.cla()
-
-        # Plot Episode Durations
-        axs[0].set_title('Training Metrics')
-        axs[0].set_ylabel('Duration')
-        durations_t = torch.tensor(episode_durations, dtype=torch.float)
-        axs[0].plot(durations_t.numpy(), label='Raw')
-        smoothed_durations = np.cumsum(durations_t.numpy()) / (np.arange(len(durations_t)) + 1)
-        axs[0].plot(smoothed_durations, color='orange', label='Smoothed')
-        axs[0].legend()
-
-        # Plot Losses
-        axs[1].set_ylabel('Loss')
-        losses_t = np.array(losses)
-        axs[1].plot(losses_t, label='Raw')
-        smoothed_losses = np.cumsum(losses_t) / (np.arange(len(losses_t)) + 1)
-        axs[1].plot(smoothed_losses, color='orange', label='Smoothed')
-        axs[1].legend()
-
-        # Plot Epsilon Thresholds
-        axs[2].set_ylabel('Epsilon')
-        axs[2].plot(eps_thresholds, label='Epsilon')
-        axs[2].legend()
-
-        # Plot Reward
-        axs[3].set_xlabel('Episode')
-        axs[3].set_ylabel('Reward')
-        rewards_t = torch.tensor(episode_rewards, dtype=torch.float)
-        axs[3].plot(rewards_t.numpy(), label='Episode Reward')
-        axs[3].legend()
-
-        plt.tight_layout()
-        plt.pause(0.001)  # pause a bit so that plots are updated
-
+    
     num_episodes = 25000
     for i_episode in range(num_episodes):
         state, info = env.reset()
@@ -193,7 +149,7 @@ def objective(trial):
             if average_reward > PERFORMANCE_THRESHOLD:
                 EPS_START = max(EPS_START * (1-EPS_DECAY), EPS_END)
 
-        plot_durations(episode_durations, losses, eps_thresholds, episode_rewards, optimization_mode=False)
+        plot_function(episode_durations, losses, eps_thresholds, episode_rewards, optimization_mode=False)
 
         trial.report(episode_durations[-1], i_episode)
 
